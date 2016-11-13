265c182c60c8407413d8c4bc69aa985977fcf061
mekoo <michael.jespersen@gmail.com>
2016-11-13 18:32:30 +0100

    added deleted package.json

--- src/app/decisionspace/toolbar/visControls/visCtrl.component.ts
+++ src/app/decisionspace/toolbar/visControls/visCtrl.component.ts
@@ -4,11 +4,7 @@ import { MakeDraggable } from '../../shared/draggable/make-draggable.directive';
 
 
 @Component({
-<<<<<<< HEAD:src/app/decisionspace/toolbar/visControls/visCtrl.component.ts
   selector: 'udm-visctrl',
-=======
-  selector: 'ud2d-visctrl',
->>>>>>> fdbe35102fad73310ae0e9ce741a03ef926e09e5:src/app/decisionspace/toolbar/visControls/visCtrl.component.ts
   template: `
     <p [makeDraggable]="item"><li>{{item.name}}</li></p>
   `
